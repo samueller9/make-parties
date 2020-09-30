45ea9e7eb0238d9e6654a6498fd43a25571ad816
samueller9 <60405236+samueller9@users.noreply.github.com>
2020-09-30 15:50:21 -0700

    Users can edit and update events

--- app.js
+++ app.js
@@ -1,35 +1,23 @@
+
 // Initialize express
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 3000;
-const Handlebars = require('handlebars');
+const express = require('express');
+const Handlebars = require("handlebars");
 const { allowInsecurePrototypeAccess } = require('@handlebars/allow-prototype-access');
+const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
 const models = require('./db/models');
+const methodOverride = require('method-override')
 
-// Tell the app what port to listen on
-app.listen(port, () => {
-  console.log('App listening on port 3000!')
-})
-
-// require handlebars
-const exphbs = require('express-handlebars');
+const app = express();
 
 // Use "main" as our default layout
 app.engine('handlebars', exphbs({ defaultLayout: 'main', handlebars: allowInsecurePrototypeAccess(Handlebars) }));
 // Use handlebars to render
 app.set('view engine', 'handlebars');
-
-
-// // OUR MOCK ARRAY OF PROJECTS
-// var events = [
-//   { title: "I am your first event", desc: "A great event that is super fun to look at and good", imgUrl: "https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn" },
-//   { title: "I am your second event", desc: "A great event that is super fun to look at and good", imgUrl: "https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn" },
-//   { title: "I am your third event", desc: "A great event that is super fun to look at and good", imgUrl: "https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn" }
-// ]
-
 // The following line must appear AFTER const app = express() and before your routes!
 app.use(bodyParser.urlencoded({ extended: true }));
+// override with POST having ?_method=DELETE or ?_method=PUT
+app.use(methodOverride('_method'))
 
 // INDEX
 app.get('/', (req, res) => {
@@ -61,4 +49,30 @@ app.get('/events/:id', (req, res) => {
     // if they id was for an event not in our db, log an error
     console.log(err.message);
   })
+})
+
+// EDIT
+app.get('/events/:id/edit', (req, res) => {
+  models.Event.findByPk(req.params.id).then((event) => {
+    res.render('events-edit', { event: event });
+  }).catch((err) => {
+    console.log(err.message);
+  })
+});
+// UPDATE
+app.put('/events/:id', (req, res) => {
+  models.Event.findByPk(req.params.id).then(event => {
+    event.update(req.body).then(event => {
+      res.redirect(`/events/${req.params.id}`);
+    }).catch((err) => {
+      console.log(err);
+    });
+  }).catch((err) => {
+    console.log(err);
+  });
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log('App listening on port 3000!')
 })
