c0bd42f70880bedec4f37f483346369e1ef6064b
samueller9 <60405236+samueller9@users.noreply.github.com>
2020-10-06 15:31:26 -0700

    update

--- db/models/event.js
+++ db/models/event.js
@@ -3,25 +3,22 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  // class Event extends Model {
+  class Event extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    // static associate(models) {
+    static associate(models) {
       // define association here
-  //   }
-  // };
-
-  const Event = sequelize.define('Event', {
+    }
+  };
+  Event.init({
     title: DataTypes.STRING,
-    desc: DataTypes.TEXT,
-    imgUrl: DataTypes.STRING //add this line (don't forget the comma above!)
-  }, {});
-
-  //   sequelize,
-  //   modelName: 'Event',
-  // });
+    desc: DataTypes.TEXT
+  }, {
+    sequelize,
+    modelName: 'Event',
+  });
   return Event;
 };
